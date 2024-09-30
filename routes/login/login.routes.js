5eb01dd1ae4012a903e886936b51746ba0be7444
soka1998 <soukainaharifi18@gmail.com>
2024-09-30 03:51:41 +0100

    initial commit

--- routes/login/login.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const login = require("../../controllers/login/login.controller.js");
-
-// Create a new user
-router.post('/', login.authenticate);
-
-module.exports = router;
--- routes/register/register.routes.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-const register = require("../../controllers/register/register.controller.js");
-
-// Create a new user
-router.post('/', register.create);
-
-module.exports = router;
